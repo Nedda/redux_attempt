4f078a751d3b2b9716949c24e94cedc548378987
Nedda Amini <nedda.amini@gmail.com>
2016-06-03 15:00:41 -0700

    everything is a mess, everything is on fire how did I do this?

---
+++ src/TodoList.js
@@ -0,0 +1,25 @@
+import React, { PropTypes } from 'react';
+import Todo from './Todo.js';
+
+const TodoList = ({ todoArray, onTodoClick }) => (
+    <ul>
+        {todoArray.map(todo => 
+           <Todo 
+                onClick={() => onTodoClick(todo.id)}
+                completed={todo.completed}
+                text={todo.text}/>
+          )}
+    </ul>
+);
+
+
+TodoList.propTypes = {
+    todoArray: PropTypes.arrayOf(PropType.shape({
+        id: PropTypes.num.isRequired,
+        completed: PropTypes.bool.isRequired,
+        text: PropTypes.string.isRequired
+    }).isRequired).isRequired,
+    onTodoClick: PropTypes.func.jsRequired
+}
+
+export default TodoList;
